3b9e98c3ce821998c05e830046e2251935f75901
Burak <6028083@mborijnland.nl>
2024-03-26 12:10:27 +0100

    burakbranch

---
+++ src/js/planten.class.js
@@ -0,0 +1,47 @@
+dataHardObject = [
+    {
+        "id": 1,
+        "plantNaam": "Tomt",
+        "plantensoort": "Groente",
+        "plantGeteelt": 1
+    },
+    {
+        "id": 2,
+        "plantNaam": "Komkommer",
+        "plantensoort": "Groente",
+        "plantGeteelt": 1
+    }
+]
+
+class Plant {
+    constructor(dataObject) 
+    {
+      this.id = dataObject.id;
+      this.plantNaam = dataObject.plantNaam;
+      this.plantensoort = dataObject.plantensoort;
+      this.plantGeteelt = dataObject.plantGeteelt;
+    }
+
+    htmlData() {
+        if (this.plantGeteelt) 
+        {
+            const titel = document.querySelectorAll(".plant-naam");
+            for (let i = 0; i < titel.length; i++) {
+                const element = titel[i];
+                element.textContent = this.plantNaam;
+            }
+        }
+    }
+}
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const tomaatPlant = new Plant(dataHardObject[0]);
+//     tomaatPlant.htmlData();
+// });
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    dataHardObject.forEach(plantObject => {
+        const plant = new Plant(plantObject);
+        plant.htmlData();
+    });
+})
